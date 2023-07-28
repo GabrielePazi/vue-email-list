9895e43408219c414ae47dcfe00a2520ad773e51
GabrielePazi <pazigabriele@gmail.com>
2023-07-28 16:19:00 +0200

    1.0 esercizio completato(no bonus)

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const app = Vue.createApp({
+  data() {
+    return {
+      randomMails: []
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((axiosResp) => {
+          let newMail = axiosResp.data.response
+
+          this.randomMails.push(newMail)
+        })
+    }
+  }
+}).mount('#app')
